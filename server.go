5a4c106e0f187d197ec2a9c9eb89a67322a789b0
Pongpipat Poolkhaolan <pongpipat.po@kbtg.tech>
2020-09-26 09:54:28 +0700

    add

---
+++ server.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+
+	_ "github.com/lib/pq"
+	"go.uber.org/zap"
+)
+
+func main() {
+	fmt.Println("hello hometic : I'm Gopher!!")
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := mux.NewRouter()
+	r.Handle("/pair-device", PairDeviceHandler(NewCreatePairDevice(db))).Methods(http.MethodPost)
+
+	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
+	fmt.Println("addr:", addr)
+
+	server := http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+
+	log.Println("starting...")
+	log.Fatal(server.ListenAndServe())
+}
+
+type Pair struct {
+	DeviceID int64
+	UserID   int64
+}
+
+func PairDeviceHandler(device Device) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		
+		l:=zap.NewExample()
+		l = l.With(zap.Namespace(key:"hometic"), zap.String(key:"I'm", val:"disya")) //return ของใหม่ ไม่ใช้ l ตัวเดิม
+	l.Info(msg:"pair-device")
+}
+
+		var p Pair
+		err := json.NewDecoder(r.Body).Decode(&p)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(err.Error())
+			return
+		}
+		defer r.Body.Close()
+		fmt.Printf("pair: %#v\n", p)
+
+		err = device.Pair(p)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(err.Error())
+			return
+		}
+
+		w.Write([]byte(`{"status":"active"}`))
+	}
+}
+
+type Device interface {
+	Pair(p Pair) error
+}
+
+type CreatePairDeviceFunc func(p Pair) error
+
+func (fn CreatePairDeviceFunc) Pair(p Pair) error {
+	return fn(p)
+}
+
+func NewCreatePairDevice(db *sql.DB) CreatePairDeviceFunc {
+	return func(p Pair) error {
+		_, err := db.Exec("INSERT INTO pairs VALUES ($1,$2);", p.DeviceID, p.UserID)
+		return err
+
+	}
+}
